167978d56245912f4cf2ea22319cbf7ea50642a1
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-02-07 15:45:50 +0000

    Use upstream docker-compose

--- install.sh
+++ install.sh
@@ -36,7 +36,8 @@ apt install -y docker-ce
 
 usermod -aG docker $USER
 
-apt install -y docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 # Install Python Environment Tools
 echo "################ Installing Python ################"
